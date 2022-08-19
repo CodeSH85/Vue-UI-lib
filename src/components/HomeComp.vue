e91a5018b280a73a708c092a9668914636bd4a41
CodeSH85 <codesh85@gmail.com>
2022-08-19 10:55:40 +0800

    init

---
+++ src/components/HomeComp.vue
@@ -0,0 +1,16 @@
+<script setup>
+import AqiBarChart from "./baseComponents/Chart.vue"
+import infoCard from "./baseComponents/infoCard.vue"
+
+</script>
+<template>
+  <div>
+    <infoCard/>
+    <AqiBarChart/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import AqiBarChart from "../components/baseComponents/Chart.vue"
+import HomeComp from "../components/HomeComp.vue"
+
+</script>
+<template>
+  <HomeComp/>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
