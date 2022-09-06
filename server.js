12d8f46830c7945e7c0b0e307c2810db077eff90
CodeSH85 <codesh85@gmail.com>
2022-09-06 14:09:41 +0800

    add server and local api

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 
 const Express = require('Express');
+require('dotenv').config()
 
 const app = new Express;
 
 const port = 3006;
 
-app.get('/api', (req, res)=>{
-  const url = ''
+// app.get('/api', async (req, res) => {
+//   const data = await fetch(process.env.BASE_URL)
+//   res = data.json() 
+// })
+
+app.get('/', (req, res) => {
+  // const data = fetch(process.env.BASE_URL)
+  // res = data.json() 
+  res.send('hello')
 })
 
+app.get('/api', async (req, res) => {
+  const data = await fetch(process.env.BASE_URL)
+  const json = await data.json()
+  // console.log(json)
+  await res.status(200).send(json)
+})
 
 app.listen(port,()=>{
   console.log(`server is running on ${port}`);
