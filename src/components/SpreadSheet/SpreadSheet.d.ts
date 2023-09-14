7df0e00a0ed01c09ce39c0f78f00d8e42b3d2aba
CodeSH85 <codesh85@gmail.com>
2023-09-14 22:47:02 +0800

    add SpreadSheet

---
+++ src/components/SpreadSheet/SpreadSheet.d.ts
@@ -0,0 +1,5 @@
+export interface Headers {
+  key: string;
+  title: string;
+  type?: string;
+}
---
+++ src/components/SpreadSheet/SpreadSheet.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div
+      class="header"
+    >
+      <span
+        v-for="(col, c) in columns" :key="c"
+      >
+        {{ col.title }}
+      </span>
+    </div>
+    <div
+      class=""
+      v-for="(row, r) in rows" :key="r"
+      >
+      <span
+        v-for="(col, c) in columns" :key="c"
+      >
+        <input :value="row[col.key]"/>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue'
+import Headers from './SpreadSheet'
+
+const props = defineProps({
+  headers: {
+    type: Array as PropType<Headers[]>,
+    default: () => []
+  },
+  data: {
+    type: Array,
+    default: () => []
+  }
+})
+
+const columns = props.headers
+const rows = props.data
+
+</script>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+}
+</style>
