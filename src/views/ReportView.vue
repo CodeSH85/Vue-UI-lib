eefe3c6207fe200bac5471e3eb785544984d5f78
CodeSH85 <codesh85@gmail.com>
2023-09-23 22:12:40 +0800

    add Tab component

---
+++ src/views/ReportView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <TabView></TabView>
+  </div>
+</template>
+
+<script setup lang="ts">
+import TabView from '../components/TabView.vue'
+</script>
+
+<style scoped lang="scss">
+
+</style>
