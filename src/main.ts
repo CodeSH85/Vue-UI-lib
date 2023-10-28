cd3ff34e628c94f07b69a6cf02275b7c50512526
CodeSH85 <codesh85@gmail.com>
2023-10-28 22:42:13 +0800

    pinia

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { createApp } from 'vue'
 import './assets/style.scss'
 import App from './App.vue'
 import router from './router/index.js'
+import { createPinia } from 'pinia'
+const pinia = createPinia()
 // import feather from 'feather-icons'
 
 createApp(App)
   .use(router)
+  .use(pinia)
   // .use(feather)
   .mount('#app')
